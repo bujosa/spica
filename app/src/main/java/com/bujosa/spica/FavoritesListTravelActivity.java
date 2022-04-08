69cc79585d2503a59ee9bb71898059887becc4c5
David Bujosa <davidbujosa@gmail.com>
2022-04-08 14:32:24 +0200

    feat: create favorite list travel activity

---
+++ app/src/main/java/com/bujosa/spica/FavoritesListTravelActivity.java
@@ -0,0 +1,36 @@
+package com.bujosa.spica;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bujosa.spica.adapter.TravelAdapter;
+import com.bujosa.spica.entity.Travel;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoritesListTravelActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.available_travels);
+        recyclerView=findViewById(R.id.listRecyclerView);
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("mytravels", null);
+        Type type = new TypeToken<ArrayList<Travel>>() {}.getType();
+        List<Travel> travels = gson.fromJson(json, type);
+        recyclerView.setAdapter(new TravelAdapter(travels, this, false));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+    }
+}
