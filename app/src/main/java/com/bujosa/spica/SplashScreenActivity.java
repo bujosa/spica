a33b29dddeae8a35ee6c316f9d23b64893e8994b
David Bujosa <davidbujosa@gmail.com>
2022-04-08 19:58:08 +0200

    fix: refactor splash screen

--- app/src/main/java/com/bujosa/spica/SplashScreenActivity.java
+++ app/src/main/java/com/bujosa/spica/SplashScreenActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.splashScreenTheme);
-        SystemClock.sleep(1000);
-        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+            }
+        },3000);
     }
 }
